82bc9d04d9ad8da6882b72c0e6fa7b2643cd1e65
Sebastian <sebastian.c@sdsharp.com>
2023-09-04 19:44:56 -0600

    [ref] Atomo script

--- Assets/Scripts/Scripts Clase 3/Atomo.cs
+++ Assets/Scripts/Scripts Clase 3/Atomo.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Atomo : MonoBehaviour
 {
     public Transform center; // El objeto central alrededor del cual giran los electrones.
-    public float orbitSpeed = 50f; // Velocidad de órbita.
-    public float orbitRadius = 2f; // Radio de la órbita.
+    public float orbitSpeed = 50f; // Velocidad de ï¿½rbita.
+    public float orbitRadius = 2f; // Radio de la ï¿½rbita.
 
     private void Update()
     {
-        // Rotar el objeto central (núcleo) si se desea.
+        // Rotar el objeto central (nï¿½cleo) si se desea.
         center.Rotate(Vector3.up * orbitSpeed);
+        center.Rotate(Vector3.up * orbitSpeed);
+        center.Rotate(Vector3.up * orbitSpeed);
+        
 
         
     }

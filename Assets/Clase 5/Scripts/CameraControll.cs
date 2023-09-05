11301d9224a7ca8285bf8e5a5611921c6541b0d3
Sebastian <sebastian.c@sdsharp.com>
2023-09-04 19:53:52 -0600

    Camera controll add comment

--- Assets/Clase 5/Scripts/CameraControll.cs
+++ Assets/Clase 5/Scripts/CameraControll.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class CameraControll : MonoBehaviour
 {
     // Start is called before the first frame update
+    // Update Git Camera Controller
 
     [SerializeField]
     List<GameObject> _cameras;
